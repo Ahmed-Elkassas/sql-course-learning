ae117e7283ee34edeb6149513d4e530a4abd14e4
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-01 15:17:21 +0200

    Update full_name, current_status column with NOT NULL constrain

--- working-with-data/insert-users-data.sql
+++ working-with-data/insert-users-data.sql
@@ -1,2 +1,5 @@
-INSERT INTO users (yearly_salary, full_name, current_status)
-VALUES (20000, 'Ahmed Elkassas', 'employed'),(19000, 'Ali Mahmoud', 'self-employed')
+-- INSERT INTO users (yearly_salary, full_name, current_status)
+-- VALUES (20000, 'Ahmed Elkassas', 'employed'),(19000, 'Ali Mahmoud', 'self-employed')
+
+INSERT INTO users (yearly_salary, current_status)
+VALUES (20000, 'unemployed')
