cb4c20ee51b6860c64863968ae200783e8f5f9e9
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-01 15:12:16 +0200

    Re-create employers table with ID - mysql system

--- working-with-data/recreate-tables-with-id.sql
+++ working-with-data/recreate-tables-with-id.sql
@@ -4,8 +4,16 @@ DROP TABLE conversations;
 
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO-INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(300) NOT NULL,
     yearly_salary INT CHECK (yearly_salary > 0),
     current_status ENUM('self-employed', 'employed', 'unemployed')
+)
+
+CREATE TABLE employers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue FLOAT CHECK (yearly_revenue > 0),
+    is_hiring BOOLEAN DEFAULT FALSE
 )
