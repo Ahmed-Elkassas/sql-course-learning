bc4240a4d04738497d1d33d77d83d3ac4fbf90e0
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-22 14:41:22 +0200

    Insert data into tables

---
+++ relations/02-insert-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, birthday, email)
+VALUES ('Ahmed', 'Elkassas', '1994-09-01', 'contact@ahmedelkassas.com');
+
+INSERT INTO intranet_accounts (email, password)
+VALUES ('contact@ahmedelkassas.com', 'Password#21');
