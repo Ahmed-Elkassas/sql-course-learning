8088ebed91b9a047e519f2b656772cb976330b57
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-24 13:01:16 +0200

    Insert data into table with supervisor_id data

--- relations/08-Self-Referencing.sql
+++ relations/08-Self-Referencing.sql
@@ -1,6 +1,11 @@
-CREATE TABLE employees_ref (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(200) NOT NULL,
-    email VARCHAR(300) UNIQUE NOT NULL,
-    supervisor_id INT REFERENCES employees_ref (id) ON DELETE SET NULL
-)
+-- CREATE TABLE employees_ref (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(200) NOT NULL,
+--     email VARCHAR(300) UNIQUE NOT NULL,
+--     supervisor_id INT REFERENCES employees_ref (id) ON DELETE
+--     SET NULL
+-- );
+INSERT INTO employees_ref (first_name, email, supervisor_id)
+Values ('Ahmed', 'ahmed@test.com', Null),
+    ('Mohamed', 'mohamed@t.com', 1),
+    ('Ali', 'ali@test.com', 2);
