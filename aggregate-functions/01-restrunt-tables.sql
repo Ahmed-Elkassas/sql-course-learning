d7302e40cfb0fef5646e94923b229146990ab022
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-24 13:43:01 +0200

    Create new database and add 2 tables to it

---
+++ aggregate-functions/01-restrunt-tables.sql
@@ -0,0 +1,14 @@
+-- CREATE DATABASE restaurants;
+
+CREATE TABLE payment_methods (
+    -- id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200)
+);
+
+CREATE TABLE tables (
+--  id INT PRIMARY KEY AUTO_INCREMENT,
+ id SERIAL PRIMARY KEY,
+    num_seats INT,
+    category VARCHAR(200)
+);
