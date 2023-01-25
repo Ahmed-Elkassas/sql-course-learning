b365d391fa2e60805f4e8445d823fd1c616ba6b1
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-25 18:51:55 +0200

    Get the date of MIN amount_billed with nested-subquery and HAVING filter

--- aggregate-functions/06-nested-subquery.sql
+++ aggregate-functions/06-nested-subquery.sql
@@ -1,4 +1,8 @@
-SELECT MIN(daily_sum) FROM (
+SELECT booking_date FROM bookings
+GROUP BY booking_date
+HAVING SUM(amount_billed) = (
+    SELECT MIN(daily_sum) FROM (
     SELECT booking_date, SUM(amount_billed) AS daily_sum FROM bookings
     GROUP BY booking_date
-) AS daily_table;
+    ) AS daily_table
+);
