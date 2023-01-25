cee3aa953c7b9cca40608c7c28bf5c2379386e7e
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-25 12:38:55 +0200

    Query data with aggregate function and where filter

--- aggregate-functions/05-aggregate-non aggregate-fun.sql
+++ aggregate-functions/05-aggregate-non aggregate-fun.sql
@@ -1,2 +1,8 @@
-SELECT booking_date, SUM(num_guests) FROM bookings
+-- SELECT booking_date, SUM(num_guests) FROM bookings
+-- GROUP BY booking_date;
+
+
+SELECT booking_date, COUNT(booking_date) 
+FROM bookings
+WHERE amount_billed > 30
 GROUP BY booking_date;
