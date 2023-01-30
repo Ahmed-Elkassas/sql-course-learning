3f4c34ece60e89c5475f05aebf73466232a5e68b
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-30 15:11:46 +0200

    Query data with mathematical functions

---
+++ Built-in Query Functions & Operators/03-mathematical-functions.sql
@@ -0,0 +1,7 @@
+-- SELECT SUM(price * billing_frequency) FROM memberships;
+
+
+-- SELECT ROUND(consumption, 1) 
+-- SELECT TRUNCATE(consumption, 2)  -- MySQL
+SELECT TRUNC(consumption, 1)   -- Postgres
+FROM memberships;
