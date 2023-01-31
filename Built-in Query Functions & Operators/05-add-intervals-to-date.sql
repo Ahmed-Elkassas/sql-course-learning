4134f36558bfc1d11ab8359f24ba308362dc6a6e
Ahmed-Elkassas <ahmedelkassas2811@gmail.com>
2023-01-31 07:18:14 +0200

    Add more INTERVAL with Postgres

--- Built-in Query Functions & Operators/05-add-intervals-to-date.sql
+++ Built-in Query Functions & Operators/05-add-intervals-to-date.sql
@@ -1,3 +1,5 @@
-SELECT membership_start + 5, membership_start FROM memberships; -- Postgres
+-- SELECT membership_start + 5, membership_start FROM memberships; -- Postgres
 
-SELECT DATE_ADD(membership_start, INTERVAL 5 DAY), membership_start FROM memberships; -- MySQL
+-- SELECT DATE_ADD(membership_start, INTERVAL 5 DAY), membership_start FROM memberships; -- MySQL
+
+-- SELECT (membership_start + INTERVAL '6 DAY')::TIMESTAMP::DATE, membership_start FROM memberships; -- Postgres
